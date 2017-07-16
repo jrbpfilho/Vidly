1e628ec3698bf2957e9de09f57a29654ce752b11
Ruy Borges <jrbpfilho@gmail.com>
2017-07-16 14:40:34 -0700

    Initial Commit with Entity

---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+
+        public ViewResult Index()
+        {
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie {Id = 1, Name = "Shrek!"},
+                new Movie {Id = 1, Name = "Hangover"},
+                new Movie {Id = 1, Name = "Titanic"},
+                new Movie {Id = 1, Name = "Hangover 2"}
+
+            };
+        }
+
+        // GET: Movies
+        public ViewResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2"}
+            };
+
+            var viewModel = new RandomMovieViewModel
+
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+
+        }
+    }
+}
