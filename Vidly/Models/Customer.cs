7b468ec44961ef4306da83ddd298d21bea7b0b37
Ruy Borges <jrbpfilho@gmail.com>
2017-07-26 16:00:33 -0700

    Client Validation Chapter

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,18 +11,20 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
 
-        [Required]
+        [Required(ErrorMessage ="Please enter customer's name.")]
         [StringLength(255)]
         public string Name { get; set; }
 
         public bool IsSubscribedToNewsletter { get; set; }
 
+        
         public MembershipType MembershipType { get; set; }
 
         [Display(Name = "Membership Type")]
-        public byte MembershipTypeId { get; set; }
+        public byte? MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? BirthDate { get; set; }
     }
 }
